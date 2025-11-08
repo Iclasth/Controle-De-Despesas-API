e1cc325aca1f9f1bde478d5d601d89d5405fa554
iclasTH <iclasinprogramacao@gmail.com>
2025-11-08 00:29:31 -0300

    Configurando o DB context, as entidades(models) e a validação de dados com Pydantic

---
+++ API-Despesas-Pessoais/app/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from app.db import Base 
+
+class Despesa(Base): 
+    __tablename__ = "despesas"
+
+    id_despesa = Column(Integer, primary_key=True, index=True)
+    desc_despesa = Column(String, nullable=False)
+    valor = Column(Float, nullable=False)
+    data_despesa = Column(Date, nullable= False)
+    categoria = Column(String, nullable=True)
+    tipo_despesa = Column(String, default="Variável")  
---
+++ API-Despesas-Pessoais/app/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel, Field, validator
+from datetime import date
+from typing import Optional
+
+CATEGORIAS_PERMITIDAS = {"Alimentação, Transporte, Lazer, Saúde, Moradia, Outros"}
+
+class DespesaBase(BaseModel):
+    categoria: str
+    valor: float = Field(..., gt=0, description="Valor da despesa não pode ser negativo.")
+    data_despesa: date
+    desc_despesa: Optional[str] = None
+    tipo_despesa: Optional[str] = "Variável"
+
+    @validator("categoria")
+    def check_categoria(cls, value):
+        if value not in CATEGORIAS_PERMITIDAS:
+            raise ValueError(f"A categoria deve estar entre as categorias seguintes: {sorted(CATEGORIAS_PERMITIDAS)}")
+        return value
+    
+    @validator("tipo_despesa")
+    def check_tipo_despesa(cls, value):
+        if value not in {"Fixa", "Variável"}:
+            raise ValueError("O tipo de despesa deve ser 'fixa' ou 'variavel'")
+        return value
+    
+class DespesaCreate(DespesaBase):
+    pass
+
+class DespesaUpdate(BaseModel):
+    categoria: Optional[str]
+    valor: Optional[float]
+    data_despesa: Optional[date]
+    desc_despesa: Optional[str]
+    tipo_despesa: Optional[str]
+
+class DespesaOut(DespesaBase):
+    id_despesa: int
+
+    class Config:
+        orm_mode = True
